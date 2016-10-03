fde06c8cae1504723d6226c635c55e846b7b2864
Sangheun <heun0108@gmail.com>
2016-10-03 21:30:22 +0900

    input/output 수정

--- homework/2016-09-28/homework_1.py
+++ homework/2016-09-28/homework_1.py
@@ -1,5 +1,8 @@
 # 1. 구구단
-num = int(input("input number: "))
-for i in range(1,10):
-    result = '{} x {} = {}'.format(num, i, i*num)
-    print(result)
+num = int(input("숫자를 입력해 주세요 "))
+if num < 10:
+    for i in range(1,10):
+        result = '{} x {} = {}'.format(num, i, i*num)
+        print(result)
+else:
+    print("9 이하의 숫자를 입력하셔야 합니다.")
--- homework/2016-09-28/homework_3.py
+++ homework/2016-09-28/homework_3.py
@@ -2,12 +2,12 @@
 color_list = ['red', 'green', 'blue', 'blue', 'green', 'black', 'black', 'lime']
 colors = ','.join(color_list)
 
-print('color list: {}'.format(colors))
-selected_color = input("input color: ")
+print('색깔 리스트: {}'.format(colors))
+selected_color = input("색깔 리스트 중에서 색깔을 하나 골라서 입력해 주세요: ")
 
 count = 0
 for color in color_list:
     if color == selected_color:
         count += 1
 
-print('color count: {}'.format(count))
+print('{}은/는 {}개 있습니다.'.format(selected_color, count))
